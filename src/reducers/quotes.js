46bed03acbff4c81476e0fc64bb028148bde3925
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-25 17:09:03 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -3,6 +3,12 @@ export default (state = [], action) => {
   let quote; 
   
   switch(action.type){
+    case "ADD_QUOTE": 
+      return state.concat(action.quote); 
+    case "REMOVE_QUOTE": 
+      return state.filter(quote => quote.id !== action.quoteId); 
+    case "UPVOTE_QUOTE"
+      return 
     
   }
 }
