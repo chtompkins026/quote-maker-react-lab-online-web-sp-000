2743b26eac8a55b0caeb8a46ba64c1164fd5fe25
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-25 17:16:11 +0000

    Automatically backed up by Learn

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -2,24 +2,37 @@ export default (state = [], action) => {
   let index;
   let quote;
 
-  switch(action.type){
-    case "ADD_QUOTE":
-      return state.concat(action.quote)
+  switch (action.type) {
 
-    case "REMOVE_QUOTE":
-      return state.filter(quote => quote.id !== action.quoteId)
+    case 'ADD_QUOTE':
+      return state.concat(action.quote);
 
-    case "UPVOTE_QUOTE":
-      index = state.findIndex(quote => quote.id === action.quoteId)
-      quote = state[index]
-      return [...state.slice(0,index),{...quote, votes: quote.votes+= 1},...state.slice(index+1)]
+    case 'REMOVE_QUOTE':
+      return state.filter(quote => quote.id !== action.quoteId);
 
-    case "DOWNVOTE_QUOTE":
-    index = state.findIndex(quote => quote.id === action.quoteId)
-    quote = state[index]
-    return quote.votes > 0 ? [...state.slice(0,index),{...quote, votes: quote.votes-= 1},...state.slice(index+1)] : state
+    case 'UPVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
 
-    default:
-      return state
+      return [
+        ...state.slice(0, index),
+        Object.assign({}, quote, { votes: quote.votes += 1 }),
+        ...state.slice(index + 1)
+      ];
+
+    case 'DOWNVOTE_QUOTE':
+      index = state.findIndex(quote => quote.id === action.quoteId);
+      quote = state[index];
+      if (quote.votes > 0) {
+        return [
+          ...state.slice(0, index),
+          Object.assign({}, quote, { votes: quote.votes -= 1 }),
+          ...state.slice(index + 1)
+        ];
+      }
+      return state;
+
+    default: 
+      return state;
   }
 }
