87bac900805f28758ee0efdee1bd58799d7c76f2
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-25 17:16:52 +0000

    Automatically backed up by Learn

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const QuoteCard = ({quote, removeQuote, upvoteQuote, downvoteQuote}) =>
+const QuoteCard = ({ quote, removeQuote, upvoteQuote, downvoteQuote }) => 
   <div>
     <div className="card card-inverse card-success card-primary mb-3 text-center">
       <div className="card-block">
@@ -9,26 +9,26 @@ const QuoteCard = ({quote, removeQuote, upvoteQuote, downvoteQuote}) =>
           <footer>- author <cite title="Source Title">{quote.author}</cite></footer>
         </blockquote>
       </div>
-      <div className="float-right">
+      <div className="float-right"> 
         <div className="btn-group btn-group-sm" role="group" aria-label="Basic example">
-          <button
-            type="button"
+          <button 
+            type="button" 
+            onClick={() => upvoteQuote(quote.id)} 
             className="btn btn-primary"
-            onClick={upvoteQuote(quote.id)}
           >
             Upvote
           </button>
-          <button
-            type="button"
+          <button 
+            type="button" 
+            onClick={() => downvoteQuote(quote.id)} 
             className="btn btn-secondary"
-            onClick={downvoteQuote(quote.id)}
           >
             Downvote
           </button>
-          <button
-            type="button"
+          <button 
+            type="button" 
+            onClick={() => removeQuote(quote.id)} 
             className="btn btn-danger"
-            onClick={removeQuote(quote.id)}
           >
             <span aria-hidden="true">&times;</span>
           </button>
