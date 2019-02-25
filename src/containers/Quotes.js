04ac482e958d6239f772ee13915a36807c4cd075
Christopher Tompkins <chtompkins026@gmail.com>
2019-02-25 17:09:58 +0000

    Automatically backed up by Learn

--- src/containers/Quotes.js
+++ src/containers/Quotes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import QuoteCard from '../components/QuoteCard';
-import {removeQuote, upvoteQuote, down
+import { removeQuote, upvoteQuote, downvoteQuote } from '../actions/quotes';
 
 class Quotes extends Component {
 
